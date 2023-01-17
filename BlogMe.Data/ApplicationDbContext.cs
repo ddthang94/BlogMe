f449fc900d546cfc60a2463734154ae55f69ec7a
ddthang94 <ddthang21194@gmail.com>
2023-01-17 06:47:49 +0200

    Uploaded

---
+++ BlogMe.Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlogMe.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogMe.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BlogMe/Configuration/AppServices.cs
@@ -0,0 +1,22 @@
+﻿using BlogMe.Data;
+using BlogMe.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogMe.Configuration
+{
+    public static class AppServices
+    {
+        public static void AddDefaultServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+            serviceCollection.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            serviceCollection.AddControllersWithViews().AddRazorRuntimeCompilation();
+            serviceCollection.AddRazorPages();
+        }
+    }
+}
